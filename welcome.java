f675b29972f7523cef1e015bbecb1125a6a42873
bijaykath <60802489+bijaykath@users.noreply.github.com>
2020-02-28 12:05:50 -0600

    Add files via upload
    Hands on Exercise

---
+++ welcome.java
@@ -0,0 +1,16 @@
+package com.homework;
+
+public class welcome {
+    public static void main(String[] args) {
+
+        String s = "Welcome to Java World";
+        System.out.println(s.substring(5));
+        String s1 = "Welcome";
+        System.out.println(" Comparing strings with compareto:");
+        System.out.println(s.compareTo(s1));
+        System.out.print("Replacing a from e: " );
+        System.out.println(s.replace('a', 'e'));
+        System.out.println(s.substring(4,10));
+        System.out.println(s.toLowerCase());
+            }
+        }
